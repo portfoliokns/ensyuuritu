87d9445817e6b5975df0b5fad14228e1fd0e4e7c
konishi33 <100762595+konishi33@users.noreply.github.com>
2023-04-06 14:55:22 +0900

    デザインを修正

--- ensyuuritu/Form1.Designer.vb
+++ ensyuuritu/Form1.Designer.vb
@@ -33,34 +33,39 @@ Partial Class Form1
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.btnCopy = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
         Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PictureBox1.Location = New System.Drawing.Point(10, 40)
+        Me.PictureBox1.Location = New System.Drawing.Point(9, 30)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(400, 400)
+        Me.PictureBox1.Size = New System.Drawing.Size(403, 403)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(17, 14)
+        Me.Label1.Location = New System.Drawing.Point(15, 10)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(85, 20)
+        Me.Label1.Size = New System.Drawing.Size(67, 15)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "【画像描写】"
         '
         'btnStart
         '
-        Me.btnStart.Location = New System.Drawing.Point(39, 86)
+        Me.btnStart.Location = New System.Drawing.Point(34, 64)
+        Me.btnStart.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnStart.Name = "btnStart"
-        Me.btnStart.Size = New System.Drawing.Size(94, 29)
+        Me.btnStart.Size = New System.Drawing.Size(82, 22)
         Me.btnStart.TabIndex = 2
         Me.btnStart.Text = "計算開始"
         Me.btnStart.UseVisualStyleBackColor = True
@@ -68,41 +73,44 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(15, 25)
+        Me.Label2.Location = New System.Drawing.Point(13, 22)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(69, 20)
+        Me.Label2.Size = New System.Drawing.Size(55, 15)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "円周率："
         '
         'txtPi
         '
-        Me.txtPi.Location = New System.Drawing.Point(90, 21)
+        Me.txtPi.Location = New System.Drawing.Point(79, 18)
+        Me.txtPi.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.txtPi.Name = "txtPi"
-        Me.txtPi.Size = New System.Drawing.Size(144, 27)
+        Me.txtPi.Size = New System.Drawing.Size(126, 23)
         Me.txtPi.TabIndex = 4
         '
         'txtTrials
         '
-        Me.txtTrials.Location = New System.Drawing.Point(117, 41)
+        Me.txtTrials.Location = New System.Drawing.Point(102, 31)
+        Me.txtTrials.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.txtTrials.MaxLength = 200
         Me.txtTrials.Name = "txtTrials"
-        Me.txtTrials.Size = New System.Drawing.Size(144, 27)
+        Me.txtTrials.Size = New System.Drawing.Size(126, 23)
         Me.txtTrials.TabIndex = 6
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(27, 44)
+        Me.Label3.Location = New System.Drawing.Point(24, 33)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(84, 20)
+        Me.Label3.Size = New System.Drawing.Size(67, 15)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "試行回数："
         '
         'btnClear
         '
-        Me.btnClear.Location = New System.Drawing.Point(167, 86)
+        Me.btnClear.Location = New System.Drawing.Point(146, 64)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(94, 29)
+        Me.btnClear.Size = New System.Drawing.Size(82, 22)
         Me.btnClear.TabIndex = 7
         Me.btnClear.Text = "クリア"
         Me.btnClear.UseVisualStyleBackColor = True
@@ -113,9 +121,11 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.btnClear)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.btnStart)
-        Me.GroupBox1.Location = New System.Drawing.Point(450, 74)
+        Me.GroupBox1.Location = New System.Drawing.Point(433, 54)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(294, 134)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(257, 100)
         Me.GroupBox1.TabIndex = 8
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "【パラメータ】"
@@ -125,31 +135,55 @@ Partial Class Form1
         Me.GroupBox2.Controls.Add(Me.btnCopy)
         Me.GroupBox2.Controls.Add(Me.txtPi)
         Me.GroupBox2.Controls.Add(Me.Label2)
-        Me.GroupBox2.Location = New System.Drawing.Point(432, 231)
+        Me.GroupBox2.Location = New System.Drawing.Point(433, 192)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(344, 167)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox2.Size = New System.Drawing.Size(301, 51)
         Me.GroupBox2.TabIndex = 9
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "【計算結果】"
         '
         'btnCopy
         '
-        Me.btnCopy.Location = New System.Drawing.Point(240, 19)
+        Me.btnCopy.Location = New System.Drawing.Point(223, 18)
+        Me.btnCopy.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnCopy.Name = "btnCopy"
-        Me.btnCopy.Size = New System.Drawing.Size(72, 29)
+        Me.btnCopy.Size = New System.Drawing.Size(63, 22)
         Me.btnCopy.TabIndex = 8
         Me.btnCopy.Text = "コピー"
         Me.btnCopy.UseVisualStyleBackColor = True
         '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Location = New System.Drawing.Point(433, 283)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(347, 57)
+        Me.GroupBox3.TabIndex = 10
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "【円周率の求め方】"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 28)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(317, 15)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "円の面積：正方形の面積 ＝ 青色のドット：青色＋赤色のドット"
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(795, 445)
+        Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "モンテカルロシミュレーション"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -157,6 +191,8 @@ Partial Class Form1
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -173,4 +209,6 @@ Partial Class Form1
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents btnCopy As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label4 As Label
 End Class
